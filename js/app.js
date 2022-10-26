d7d905c525b6920dc900f766c574c8ab4a0cd69b
Braian Morales <braian.morales@correounivalle.edu.co>
2022-10-25 23:38:44 -0500

    add first version of the showcase

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+/** Navbar effect**/
+
+
+function semiTransparenteNavbar (){
+    //window.pageYOffset returns the position of the page on the y-axis
+    header.classList.toggle("scrolled", window.pageYOffset >0) //if y-axis potition > 0 return true
+}
+
+semiTransparenteNavbar()
+
+window.addEventListener("scroll", semiTransparenteNavbar);
